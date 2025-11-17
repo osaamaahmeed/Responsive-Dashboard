89140f142f2dad32ab0335774424751241aaa223
osaamaahmeed <iiosamaahmed1@gmail.com>
2025-11-17 10:09:51 +0200

    fixed UI bugs in Tablet Layout

--- lib/widgets/active_and_in_active_item.dart
+++ lib/widgets/active_and_in_active_item.dart
@@ -13,7 +13,7 @@ class InActiveDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(drawerItemModel.title, style: AppStyles.styleMedium16(context)),
+      title: FittedBox(fit: BoxFit.scaleDown, alignment: Alignment.centerLeft, child: Text(drawerItemModel.title, style: AppStyles.styleMedium16(context))),
     );
   }
 }
@@ -27,7 +27,7 @@ class ActiveDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(drawerItemModel.title, style: AppStyles.styleBold16(context)),
+      title: FittedBox(fit: BoxFit.scaleDown, alignment: Alignment.centerLeft, child: Text(drawerItemModel.title, style: AppStyles.styleBold16(context))),
       trailing: Container(
         width: 3.24,
         decoration: const BoxDecoration(color: Color(0xFF4EB7F2)),
--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -14,8 +14,8 @@ class UserInfoListTile extends StatelessWidget {
       child: Center(
         child: ListTile(
           leading: SvgPicture.asset(userInfoModel.image),
-          title: Text(userInfoModel.title, style: AppStyles.styleSemiBold16(context),),
-          subtitle: Text(userInfoModel.subtitle, style: AppStyles.styleRegular12(context),),
+          title: FittedBox(child: Text(userInfoModel.title, style: AppStyles.styleSemiBold16(context),)),
+          subtitle: FittedBox(child: Text(userInfoModel.subtitle, style: AppStyles.styleRegular12(context),)),
         ),
       ),
     );
